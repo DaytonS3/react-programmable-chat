e1fdc292367d6cc04028a6b80f1ef4f67d4ebd16
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-06-10 23:12:01 -0400

    big commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import MessageForm from './MessageForm'
-import MessageList from './MessageList'
-import TwilioChat from 'twilio-chat'
-import $ from 'jquery'
-import './App.css'
+import React, { Component } from "react";
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import TwilioChat from "twilio-chat";
+// import $ from "jquery";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       messages: [],
       username: null,
-      channel: null,
-    }
+      channel: null
+    };
   }
 
   componentDidMount = () => {
@@ -20,84 +21,107 @@ class App extends Component {
       .then(this.createChatClient)
       .then(this.joinGeneralChannel)
       .then(this.configureChannelEvents)
-      .catch((error) => {
-        this.addMessage({ body: `Error: ${error.message}` })
-      })
-  }
+      .catch(error => {
+        this.addMessage({ body: `Error: ${error.message}` });
+      });
+  };
 
   getToken = () => {
     return new Promise((resolve, reject) => {
-      this.addMessage({ body: 'Connecting...' })
+      this.addMessage({ body: "Connecting..." });
+      axios
+        .post("http://localhost:5000/api/token")
+        .then(res => {
+          this.setState({
+            username: res.data.identity
+          });
+          console.log(res);
+          resolve(res.data.jwt);
+        })
+        .catch(err => console.log(err));
+    });
+  };
 
-      $.getJSON('/token', (token) => {
-        this.setState({ username: token.identity })
-        resolve(token)
-      }).fail(() => {
-        reject(Error('Failed to connect.'))
-      })
-    })
-  }
-
-  createChatClient = (token) => {
+  createChatClient = token => {
     return new Promise((resolve, reject) => {
-      resolve(new TwilioChat(token.jwt))
-    })
-  }
+      console.log(token);
+      resolve(new TwilioChat(token));
+    });
+  };
 
-  joinGeneralChannel = (chatClient) => {
+  joinGeneralChannel = chatClient => {
     return new Promise((resolve, reject) => {
-      chatClient.getSubscribedChannels().then(() => {
-        chatClient.getChannelByUniqueName('general').then((channel) => {
-          this.addMessage({ body: 'Joining general channel...' })
-          this.setState({ channel })
-
-          channel.join().then(() => {
-            this.addMessage({ body: `Joined general channel as ${this.state.username}` })
-            window.addEventListener('beforeunload', () => channel.leave())
-          }).catch(() => reject(Error('Could not join general channel.')))
-
-          resolve(channel)
-        }).catch(() => this.createGeneralChannel(chatClient))
-      }).catch(() => reject(Error('Could not get channel list.')))
-    })
-  }
-
-  createGeneralChannel = (chatClient) => {
-    return new Promise((resolve, reject) => {
-      this.addMessage({ body: 'Creating general channel...' })
       chatClient
-        .createChannel({ uniqueName: 'general', friendlyName: 'General Chat' })
+        .getSubscribedChannels()
+        .then(() => {
+          chatClient
+            .getChannelByUniqueName("general")
+            .then(channel => {
+              this.addMessage({ body: "Joining general channel..." });
+              this.setState({ channel });
+
+              channel
+                .join()
+                .then(() => {
+                  this.addMessage({
+                    body: `Joined general channel as ${this.state.username}`
+                  });
+                  window.addEventListener("beforeunload", () =>
+                    channel.leave()
+                  );
+                })
+                .catch(() => reject(Error("Could not join general channel.")));
+
+              resolve(channel);
+            })
+            .catch(() => this.createGeneralChannel(chatClient));
+        })
+        .catch(() => reject(Error("Could not get channel list.")));
+    });
+  };
+
+  createGeneralChannel = chatClient => {
+    return new Promise((resolve, reject) => {
+      this.addMessage({ body: "Creating general channel..." });
+      chatClient
+        .createChannel({ uniqueName: "general", friendlyName: "General Chat" })
         .then(() => this.joinGeneralChannel(chatClient))
-        .catch(() => reject(Error('Could not create general channel.')))
-    })
-  }
+        .catch(() => reject(Error("Could not create general channel.")));
+    });
+  };
 
-  addMessage = (message) => {
-    const messageData = { ...message, me: message.author === this.state.username }
+  addMessage = message => {
+    const messageData = {
+      ...message,
+      me: message.author === this.state.username
+    };
     this.setState({
-      messages: [...this.state.messages, messageData],
-    })
-  }
+      messages: [...this.state.messages, messageData]
+    });
+  };
 
-  handleNewMessage = (text) => {
+  handleNewMessage = text => {
     if (this.state.channel) {
-      this.state.channel.sendMessage(text)
+      this.state.channel.sendMessage(text);
     }
-  }
+  };
 
-  configureChannelEvents = (channel) => {
-    channel.on('messageAdded', ({ author, body }) => {
-      this.addMessage({ author, body })
-    })
+  configureChannelEvents = channel => {
+    channel.on("messageAdded", ({ author, body }) => {
+      this.addMessage({ author, body });
+    });
 
-    channel.on('memberJoined', (member) => {
-      this.addMessage({ body: `${member.identity} has joined the channel.` })
-    })
+    channel.on("memberJoined", member => {
+      this.addMessage({ body: `${member.identity} has joined the channel.` });
+    });
 
-    channel.on('memberLeft', (member) => {
-      this.addMessage({ body: `${member.identity} has left the channel.` })
-    })
-  }
+    channel.on("memberLeft", member => {
+      this.addMessage({ body: `${member.identity} has left the channel.` });
+    });
+    channel.on("typingStarted", member => {
+      this.addMessage({ body: `${member.identity} is currently typing.` });
+    });
+  };
 
   render() {
     return (
@@ -105,8 +129,8 @@ class App extends Component {
         <MessageList messages={this.state.messages} />
         <MessageForm onMessageSend={this.handleNewMessage} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
